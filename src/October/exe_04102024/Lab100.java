f65fcc6f62318cbb183fd35c2ad3746baee0b66d
SENTHIL <senthil.cool@yahoo.com>
2024-10-23 08:53:05 +0530

    New File oct 23

--- src/October/exe_04102024/Lab100.java
+++ src/October/exe_04102024/Lab100.java
@@ -5,6 +5,12 @@ public class Lab100
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World");
+        int age=100;
+        System.out.printf("%d",age);
+        String name ="Senthil";
+        System.out.printf("%s",name);
+        boolean b =false;
+        System.out.printf("%b",b);
     }
 
 
